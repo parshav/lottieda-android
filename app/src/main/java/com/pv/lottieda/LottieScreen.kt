6d1a4f1fc894d67ea242454fe8ee510a337ea6f6
Parshav <parshav@live.com>
2019-10-28 20:42:38 -0500

    Better refresh added.

--- app/src/main/java/com/pv/lottieda/LottieScreen.kt
+++ app/src/main/java/com/pv/lottieda/LottieScreen.kt
@@ -1,18 +1,25 @@
 package com.pv.lottieda
 
 import android.view.View
-import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import com.airbnb.lottie.LottieAnimationView
+import com.airbnb.lottie.LottieComposition
+import com.airbnb.lottie.LottieCompositionFactory
+import com.jakewharton.rxbinding3.view.clicks
+import com.jakewharton.rxrelay2.BehaviorRelay
 import com.pv.base.BaseScreen
 import com.pv.base.screen
-import com.pv.base.temp
-import io.reactivex.disposables.Disposable
 import kotlinx.android.synthetic.main.screen_lottie.view.*
+import java.net.MalformedURLException
+import kotlin.Exception
 
 class LottieScreen : BaseScreen() {
 
     private lateinit var lottieView: LottieAnimationView
-    private lateinit var settingsButton: Button
+    private lateinit var refreshButton: ImageView
+    private lateinit var sourceText: EditText
 
     override fun ui() = screen {
         layout = R.layout.screen_lottie
@@ -20,13 +27,32 @@ class LottieScreen : BaseScreen() {
 
     override fun onViewLoaded(view: View) {
         lottieView = view.lottie_view
-        settingsButton = view.btn_settings
+        refreshButton = view.btn_refresh
+        sourceText = view.et_source
 
-        settingsButton.setOnClickListener {
+        baseDisposable += refreshButton
+            .clicks()
+            .map { sourceText.text.toString() }
+            .subscribe(::tryPlay)
+    }
 
-        }
+    private fun tryPlay(source: String) {
 
-        lottieView.setAnimationFromUrl("http://x.x.x.x:7777")
+        // Potentially use this for error handling
+/*        LottieCompositionFactory.fromUrl(context, source).addFailureListener {
+                Toast.makeText(
+                    context,
+                    "Error in playing URL ${it.message}",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }*/
+
+        lottieView.setAnimationFromUrl(
+            if (source.isNotEmpty() && source.first() == 'h') source
+            else "http://${source}"
+        )
         lottieView.playAnimation()
+
     }
 }
+
